186cace8c2742f531959dad64010f284377b3004
zhouqiulin <zql394@foxmail.com>
2017-03-10 17:08:12 +0800

    首次提交

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
-module.exports={
-    entry:"./src/js/main.js",
-    output:{
-        path:"./dist/js",
-        filename:"bundel.js"
-    }
+var htmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+    entry: "./src/js/main.js",
+    output: {
+        path: "./dist/js",
+        filename: "[name]-[chunkhash].js"
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: "index.html"
+        })
+    ]
 }
